213ea347af19f7d5a8fbd542b253f033013e7ee2
Levente Kapitany <levente.kapitany@logmein.com>
2022-01-18 23:41:40 +0100

    start

---
+++ pages/tervezes.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Logo from '../components/logo'
+import Banner from '../components/banner'
+import baseStyle from '../components/base.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>TripleD</title>
+        <meta name="description" content="TripleD - 3D nyomtatás" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className='header center'>
+          <div className={baseStyle.logo}>
+            <Logo/>
+          </div>
+        </div>
+
+        <div className='main'>
+          asd
+        </div>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
